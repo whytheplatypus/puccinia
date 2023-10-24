7666484446f81fe4b8c93a3a478f782d5b9ca710
David Gage <whytheplatypus@gmail.com>
2023-10-24 11:10:47 -0400

    Round of refactoring and adding the timer loop
    Continuing to use this as an experiment environment for coding with
    heavy use of a LLM, this time copilot. Largely successful at taking high
    level instruction and turning it into code for me to move around.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use clap::{Args, Parser};
 use dbus::blocking::Connection;
 use dbus_crossroads::Crossroads;
-use notify_rust::Notification;
+use notify_rust::{Notification, NotificationHandle};
 use std::{thread, time::Duration};
+
+const DBUS_NAME: &str = "com.example.Pomodoro";
+const DBUS_PATH: &str = "/com/example/Pomodoro/Timers";
 
 /// Pomodoro application that can either start a timer or initiate a server for managing timers.
 #[derive(Parser)]
@@ -34,60 +37,82 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
     match cli.subcmd {
-        SubCommand::Server(_) => {
-            Ok(run_server()?)
-        }
-        SubCommand::Start(args) => {
-            Ok(start_timer(args.time)?)
-        }
+        SubCommand::Server(_) => Ok(run_server()?),
+        SubCommand::Start(args) => Ok(start_timer(args.time)?),
     }
 }
 
-fn send_notification() -> Result<(), notify_rust::error::Error>{
-    Notification::new()
-        .summary("Pomodoro Timer")
-        .body("Time's up! Take a break.")
-        .show()?;
+struct Pomodoro {
+    notification: Notification,
+    duration: u64,
+}
+
+fn timer(minutes: u64, notification: &Notification) -> Result<(), Box<dyn std::error::Error>> {
+    let duration = Duration::new(minutes * 60, 0);
+    println!("Pomodoro started for {} minutes...", minutes);
+    let mut handle = notification.show()?;
+    let mut duration = 0;
+    while duration < minutes {
+        println!("{} minutes left...", minutes - duration);
+        thread::sleep(Duration::new(60, 0));
+        duration += 1;
+        handle.body(&format!("{} minutes left...", minutes - duration));
+        handle.update();
+    }
     Ok(())
 }
 
-fn timer(minutes: u64) -> Result<(), Box<dyn std::error::Error>> {
-    let duration = Duration::new(minutes * 60, 0);
-    println!("Pomodoro started for {} minutes...", minutes);
-    thread::sleep(duration);
-    Ok(send_notification()?)
-}
-
 fn run_server() -> Result<(), dbus::Error> {
-    // The logic for the D-Bus server remains the same as in your previous `server.rs`.
     let connection = Connection::new_session()?;
-    connection.request_name("com.example.Pomodoro", false, true, false)?;
+    connection.request_name(DBUS_NAME, false, true, false)?;
 
     let mut cr = Crossroads::new();
 
-    let iface_token = cr.register("com.example.Pomodoro", |b| {
-        b.method("Start", ("minutes",), (), move |_, _, (minutes,): (u64,)| {
-            thread::spawn(move || {
-                timer(minutes).expect("Failed to start timer");
-            });
-            Ok(())
-        });
+    let iface_token = cr.register(DBUS_NAME, |b| {
+        b.method(
+            "Start",
+            ("minutes",),
+            (),
+            move |_, _, (minutes,): (u64,)| {
+                let break_over_notification = Notification::new()
+                    .summary("Work Time Over")
+                    .body("Time for a break!")
+                    .finalize();
+
+                let work_time_over_notification = Notification::new()
+                    .summary("Break Over")
+                    .body("Time to get back to work!")
+                    .finalize();
+
+                let work_pomodoro = Pomodoro {
+                    notification: work_time_over_notification,
+                    duration: minutes,
+                };
+
+                let break_pomodoro = Pomodoro {
+                    notification: break_over_notification,
+                    duration: 2,
+                };
+
+                thread::spawn(move || {
+                    let pomodoros = [work_pomodoro, break_pomodoro];
+                    for pomodoro in pomodoros.iter().cycle() {
+                        timer(pomodoro.duration, &pomodoro.notification)
+                            .expect("Failed to start timer");
+                    }
+                });
+                Ok(())
+            },
+        );
     });
 
-    cr.insert("/com/example/Pomodoro/Timers", &[iface_token], ());
+    cr.insert(DBUS_PATH, &[iface_token], ());
     cr.serve(&connection)
 }
 
 fn start_timer(minutes: u64) -> Result<(), dbus::Error> {
-    let interface = "com.example.Pomodoro";
-    let path = "/com/example/Pomodoro/Timers";
-    // The logic for the D-Bus client remains the same as in your previous `client.rs`.
     let connection = Connection::new_session()?;
-    let proxy = connection.with_proxy(
-        "com.example.Pomodoro",
-        "/com/example/Pomodoro/Timers",
-        Duration::from_millis(5000),
-    );
+    let proxy = connection.with_proxy(DBUS_NAME, DBUS_PATH, Duration::from_millis(5000));
 
-    proxy.method_call("com.example.Pomodoro", "Start", (minutes,))
+    proxy.method_call(DBUS_NAME, "Start", (minutes,))
 }
